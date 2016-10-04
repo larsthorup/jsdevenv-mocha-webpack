685b3f8f317c4c45f7fea93b1523e0536f48e35f
Lars Thorup <lars@zealake.com>
2016-10-04 14:08:16 +0200

    sample mocha tests

---
+++ src/calc.js
@@ -0,0 +1,36 @@
+function add (a, b) {
+  return a + b;
+}
+
+function adding (a, b) {
+  return new Promise(function (resolve) {
+    resolve(a + b);
+  });
+}
+
+function addSlow (a, b, cb) {
+  cb(a + b);
+}
+
+function divide (a, b) {
+  if (b === 0) {
+    throw new Error('Cannot divide by zero');
+  }
+  return a / b;
+}
+
+function reverse (list) {
+  var result = [];
+  for (var i = list.length - 1; i >= 0; --i) {
+    result.push(list[i]);
+  }
+  return result;
+}
+
+module.exports = {
+  add: add,
+  adding: adding,
+  addSlow: addSlow,
+  divide: divide,
+  reverse: reverse
+};
---
+++ src/calc.test.js
@@ -0,0 +1,44 @@
+var calc = require('./calc');
+
+describe('calc', function () {
+  describe('add', function () {
+    it('should sum 2 and 3 to 5', function () {
+      calc.add(2, 3).should.equal(5);
+    });
+  });
+
+  describe('reverse', function () {
+    it('should turn the array upside down', function () {
+      calc.reverse([1, 2, 3]).should.deep.equal([3, 2, 1]);
+    });
+  });
+
+  describe('divide', function () {
+    it('should verify approximately one third', function () {
+      calc.divide(5, 3).should.be.approximately(1.66, 0.01);
+    });
+
+    describe('broken precondition', function () {
+      it('should throw an Error', function () {
+        (function () { calc.divide(1, 0); }).should.throw('Cannot divide by zero');
+      });
+    });
+  });
+
+  describe('addSlow', function () {
+    it('should eventually sum 2 and 3 to 5', function (done) {
+      calc.addSlow(2, 3, function (result) {
+        result.should.equal(5);
+        done();
+      });
+    });
+  });
+
+  describe('adding', function () {
+    it('should eventually sum 2 and 3 to 5', function () {
+      return calc.adding(2, 3).then(function (result) {
+        result.should.equal(5);
+      });
+    });
+  });
+});
