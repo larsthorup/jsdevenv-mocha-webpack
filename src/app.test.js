0ff65e5bf007beab87a86f6f982fa72f924125d1
Lars Thorup <lars@zealake.com>
2016-10-04 12:44:13 +0200

    dom test

--- src/app.test.js
+++ src/app.test.js
@@ -2,10 +2,23 @@ var app = require('./app');
 
 describe('app', function () {
   describe('render', function () {
-    it('should greet the world', function () {
-      var context = {};
-      app.render(context);
-      context.innerHTML.should.equal('<span>Hello World :)</span>');
+    describe('isomorphic', function () {
+      it('should greet the world', function () {
+        var context = {};
+        app.render(context);
+        context.innerHTML.should.equal('<span>Hello World :)</span>');
+      });
+    });
+
+    if (!window.isFake) describe('in browser', function () {
+      beforeEach(function () {
+        this.context = document.createDocumentFragment();
+      });
+
+      it('should greet the world', function () {
+        app.render(this.context);
+        this.context.innerHTML.should.equal('<span>Hello World :)</span>');
+      });
     });
   });
 });
